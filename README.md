e4f0cd819a5c0bcaf39c4dc8476757d1f48dd3fd
H-Abdel <H-Abdel@users.noreply.github.com>
2016-07-08 11:15:27 +0200

    first

---
+++ README.md
@@ -0,0 +1,1 @@
+A basic project which can be as a base to new ones.
---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'appli',
+    template: '<h1> Angular 2 App </h1>'
+})
+
+export class AppComponent {
+    
+ }
